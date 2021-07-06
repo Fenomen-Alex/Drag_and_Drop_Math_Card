877e4614e595d8201ab841688fcf66086c967b8f
Fenomen-Alex <alexfenomen@gmail.com>
2021-07-06 22:17:38 +0300

    Updates

---
+++ precache-manifest.7ea574931b1aee99ca4551692d416463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c81d78529edf79748e16e3464ab394",
+    "url": "/Drag_and_Drop_Math_Card/index.html"
+  },
+  {
+    "revision": "0f8e18903a61fae448d5",
+    "url": "/Drag_and_Drop_Math_Card/static/css/main.65e3d430.chunk.css"
+  },
+  {
+    "revision": "c35482d2d082e1a0f715",
+    "url": "/Drag_and_Drop_Math_Card/static/js/2.2ac276a6.chunk.js"
+  },
+  {
+    "revision": "52fc661712429cc9af7b9bed5de064e2",
+    "url": "/Drag_and_Drop_Math_Card/static/js/2.2ac276a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f8e18903a61fae448d5",
+    "url": "/Drag_and_Drop_Math_Card/static/js/main.777b02af.chunk.js"
+  },
+  {
+    "revision": "c05576532c584304e960",
+    "url": "/Drag_and_Drop_Math_Card/static/js/runtime-main.9ee448b0.js"
+  }
+]);
